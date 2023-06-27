92a0523d768751c0887a3d7f5e96f1fc686a4b10
ZeFilog <josselin.honore@protonmail.com>
2023-06-27 16:08:30 +0200

    amélioration des fonctionnalités et prise en compte de l'abscence d'option

--- scrap.py
+++ scrap.py
@@ -14,41 +14,56 @@ def search_google(query):
     driver = webdriver.Chrome(service = s,options=chrome_options)
     driver.get("https://www.google.com/search?q=" + query)
 
-    # Utilisez Selenium pour simuler l'appui sur la touche "Enter" afin d'effectuer la recherche
-    #search_box = driver.find_element(By.NAME,"q")
-    #search_box.send_keys(Keys.RETURN)
-    
     driver.implicitly_wait(10)
-    bouton_cookies = driver.find_element(By.ID, "L2AGLb")
-    bouton_cookies.click()
+    try :
+        bouton_cookies = driver.find_element(By.ID, "L2AGLb")
+        bouton_cookies.click()
+    except:
+        pass
+    driver.implicitly_wait(10)
+    try:
+        button_xpath = '//g-expandable-content[@role="button" and @tabindex="0" and @jscontroller="Ah7cLd"]'
+        button = driver.find_element(By.XPATH, button_xpath)
+        button.click()
+    except:
+        pass
 
-    driver.implicitly_wait(10)
-    button_xpath = '//g-expandable-content[@role="button" and @tabindex="0" and @jscontroller="Ah7cLd"]'
-    button = driver.find_element(By.XPATH, button_xpath)
-    button.click()
-    
-  
-    
-    # Récupérez le contenu HTML après que les contenus cachés soient disponibles
     time.sleep(2)
     html = driver.page_source
-    
-    driver.quit()  # Fermez le navigateur
+    driver.quit()  
     
     return html
 
 def extract_div_elements(html, class_names):
     soup = BeautifulSoup(html, "html.parser")
     div_elements = soup.find_all("div", class_=class_names)
-    
-    for div in div_elements:
-        print(div.text)
 
-# Demande à l'utilisateur de saisir le terme de recherche
+    results = {}
+    for i in range(0, len(div_elements), 2):
+        title = div_elements[i].text
+        description = div_elements[i+1].text
+        results[title] = description
+
+    return results
+
+def filter_results(results):
+    filtered_results = {}
+    desired_keys = ["Amazon Prime Video", "Netflix", "Disney+"]
+
+    for key, value in results.items():
+        if key in desired_keys and value == "Abonnement":
+            filtered_results[key] = value
+
+    return filtered_results
+
 query = input("Entrez votre recherche : ")
-
-# Envoie la requête de recherche et récupère les résultats
 search_results = search_google(query)
+results = extract_div_elements(search_results, ["ellip bclEt", "ellip rsj3fb"])
+filtered_results = filter_results(results)
 
-# Extrait les éléments <div> ayant la classe "ellip bclEt"
-extract_div_elements(search_results, ["ellip bclEt", "ellip rsj3fb"])
+
+if not filtered_results:
+    print("Désolé, il va falloir attendre.")
+else:
+    for key, value in filtered_results.items():
+        print(f"Ce film est disponible sur la plateforme {key} avec l'abonnement.")
