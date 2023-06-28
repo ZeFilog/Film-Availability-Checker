359c590de8bf1a9f860a47923680e08adcffcbe4
ZeFilog <josselin.honore@protonmail.com>
2023-06-28 15:58:08 +0200

    ajout de commentaire automatique

--- scrap.py
+++ scrap.py
@@ -1,3 +1,15 @@
+"""
+Film Availability Checker
+
+This program allows you to search for the availability of movies on streaming platforms. 
+You can add or remove movie names from a file and the program will search for their availability.
+
+Requirements:
+- Selenium: Python library for browser automation
+- BeautifulSoup: Python library for web scraping
+
+"""
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -6,18 +18,34 @@ from bs4 import BeautifulSoup
 import time
 
 def search_google(query):
+    """
+    Searches for a query on Google and returns the HTML content of the search results page.
+
+    Parameters:
+    - query (str): The search query to be used on Google.
+
+    Returns:
+    - html (str): The HTML content of the search results page.
+    """
+    # Configure Chrome options and set up the driver
     chrome_options = Options()
     s = Service('C:/Program Files (x86)/chromedriver')
-    driver = webdriver.Chrome(service = s,options=chrome_options)
+    driver = webdriver.Chrome(service=s, options=chrome_options)
+
+    # Perform the search on Google
     driver.get("https://www.google.com/search?q=" + query)
 
-    driver.implicitly_wait(5)
-    try :
+    # Handle any required interactions (e.g., accepting cookies) adding a try in case there is no cookie
+    driver.implicitly_wait(10)
+    try:
         bouton_cookies = driver.find_element(By.ID, "L2AGLb")
         bouton_cookies.click()
     except:
         pass
-    driver.implicitly_wait(5)
+
+    # Expand content if necessary 
+    # adding a try, if the page dont get the button or the movie title don't return a movie
+    driver.implicitly_wait(10)
     try:
         button_xpath = '//g-expandable-content[@role="button" and @tabindex="0" and @jscontroller="Ah7cLd"]'
         button = driver.find_element(By.XPATH, button_xpath)
@@ -25,13 +53,24 @@ def search_google(query):
     except:
         pass
 
+    # Wait for the page to load and retrieve the HTML content
     time.sleep(2)
     html = driver.page_source
-    driver.quit()  
-    
+    driver.quit()
+
     return html
 
 def extract_div_elements(html, class_names):
+    """
+    Extracts div elements from HTML based on the provided class names.
+
+    Parameters:
+    - html (str): The HTML content to extract elements from.
+    - class_names (list): A list of class names to filter the div elements.
+
+    Returns:
+    - results (dict): A dictionary of extracted div elements with titles as keys and descriptions as values.
+    """
     soup = BeautifulSoup(html, "html.parser")
     div_elements = soup.find_all("div", class_=class_names)
 
@@ -44,38 +83,68 @@ def extract_div_elements(html, class_names):
     return results
 
 def filter_results(results):
+    """
+    Filters the results dictionary to include only desired platform and subscription type.
+
+    Parameters:
+    - results (dict): The dictionary of movie results to filter.
+
+    Returns:
+    - filtered_results (dict): The filtered dictionary of movie results.
+    """
     filtered_results = {}
     desired_keys = ["Amazon Prime Video", "Netflix", "Disney+"]
-    desired_service = ["Abonnement","Abonnement premium"]
+    desired_service = ["Abonnement", "Abonnement premium"]
 
     for key, value in results.items():
-        if key in desired_keys and desired_service :
+        if key in desired_keys and value in desired_service:
             filtered_results[key] = value
 
     return filtered_results
 
 def read_movie_names(file_path):
+    """
+    Reads movie names from a file.
+
+    Parameters:
+    - file_path (str): The path to the file containing movie names.
+
+    Returns:
+    - movie_names (list): A list of movie names read from the file.
+    """
     with open(file_path, 'r') as file:
         movie_names = [line.strip() for line in file]
     return movie_names
 
-
-
 def add_movie_names(file_path):
+    """
+    Adds movie names to a file.
+
+    Parameters:
+    - file_path (str): The path to the file to add movie names.
+
+    """
     while True:
         movie_name = input("Entrez le nom du film à ajouter (ou 'q' pour quitter) : ")
         if movie_name == 'q':
             break
         with open(file_path, 'a') as file:
-            file.write(movie_name + '\n' )
+            file.write(movie_name + '\n')
 
 def remove_movie_names(file_path):
+    """
+    Removes movie names from a file.
+
+    Parameters:
+    - file_path (str): The path to the file to remove movie names from.
+
+    """
     movie_names = read_movie_names(file_path)
-    
+
     print("Liste des films disponibles :")
     for i, movie_name in enumerate(movie_names, start=1):
         print(f"{i}. {movie_name}")
-    
+
     while True:
         try:
             index = int(input("Entrez le numéro du film à supprimer (ou '0' pour quitter) : "))
@@ -92,6 +161,13 @@ def remove_movie_names(file_path):
             print("Entrée invalide. Veuillez entrer un numéro.")
 
 def check_movie_availability(movie_name):
+    """
+    Checks the availability of a movie on streaming platforms.
+
+    Parameters:
+    - movie_name (str): The name of the movie to check availability for.
+
+    """
     search_results = search_google(movie_name)
     results = extract_div_elements(search_results, ["ellip bclEt", "ellip rsj3fb"])
 
@@ -107,12 +183,22 @@ def check_movie_availability(movie_name):
                 print(f"{movie_name} est disponible sur la plateforme {key} avec l'abonnement.")
 
 def run_movie_search(movie_file):
+    """
+    Runs the movie search for all movie names in the specified file.
+
+    Parameters:
+    - movie_file (str): The path to the file containing movie names.
+
+    """
     movie_names = read_movie_names(movie_file)
 
     for movie_name in movie_names:
         check_movie_availability(movie_name)
 
 def main():
+    """
+    The main function of the program.
+    """
     movie_file = "films.txt"
     add_movie_names(movie_file)
     remove_movie_names(movie_file)
